de5c829a0b5a770cbcab8b5b0ee6ee10e544094a
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-17 14:26:20 -0400

    Instalando Entity Framework y creando Contexto

---
+++ PrimerParcial/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RegistroArticulos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<> { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
