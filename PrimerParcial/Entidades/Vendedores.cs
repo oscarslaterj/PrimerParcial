5f6ad745c8274f677fcf5a94af527d87a8f0de7a
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-17 18:39:15 -0400

    Poniendo Entidad en Contexto y Creando Entidad

---
+++ PrimerParcial/Entidades/Vendedores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial.Entidades
+{
+    public class Vendedores
+    {
+        [Key]
+
+        public int VendedorId { get; set; }
+        public String Nombres { get; set; }
+        public double Sueldo { get; set; }
+        public decimal Retencion { get; set; }
+
+        public Vendedores()
+        {
+            VendedorId = 0;
+            Nombres = string.Empty;
+            Sueldo = 0;
+            Retencion = 0;
+        }
+    }
+}
